58d3fa45f8fa2a47ec161802e7c4292f875c51a3
Мазур Елена <e.n.mazur@mail.ru>
2022-05-10 14:16:04 +0300

    1

---
+++ #Zadacha3.py
@@ -0,0 +1,8 @@
+#Zadacha3
+length=float(input("Введите длину прямоугольника: "))
+width=float(input("Введите ширину прямоугольника: "))
+person=int(input("введите количество"))
+d=v
+area=(length*width)
+perimeter=((length+width)*2)
+print(f"Периметр = {perimeter} см Площадь = {area} см")
