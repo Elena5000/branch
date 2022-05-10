35fd5c6477ec0871da0fbbdbcc6a817fe2b2ab81
Мазур Елена <e.n.mazur@mail.ru>
2022-05-10 14:42:39 +0300

    сложение

---
+++ #Zadacha3.py
@@ -0,0 +1,12 @@
+#Zadacha3
+from calendar import c
+
+
+length=float(input("Введите длину прямоугольника: "))
+width=float(input("Введите ширину прямоугольника: "))
+person=int(input("введите количество"))
+d=length + width
+area=(length*width)
+perimeter=((length+width)*2)
+print(f"Периметр = {perimeter} см Площадь = {area} см")
+print(d)
