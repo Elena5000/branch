f571f56fccfa1d7ae0ef8b2d058d334e454f91d3
Мазур Елена <e.n.mazur@mail.ru>
2022-05-10 13:23:45 +0300

    добавила персон

--- zadacha2.py
+++ zadacha2.py
@@ -1,5 +1,6 @@
 length=float(input("Введите длину прямоугольника: "))
 width=float(input("Введите ширину прямоугольника: "))
+person=int(input("введите количество"))
 area=(length*width)
 perimeter=((length+width)*2)
 print(f"Периметр = {perimeter} см Площадь = {area} см")
